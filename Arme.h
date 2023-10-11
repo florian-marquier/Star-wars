20673c79e6e4e642af92a70517ceb76b36aea589
florian-marquier <145351600+florian-marquier@users.noreply.github.com>
2023-10-11 14:21:47 +0200

    Add files via upload

---
+++ Arme.h
@@ -0,0 +1,19 @@
+#ifndef Arme_h
+#define Arme_h
+
+#include <iostream>
+#include <string>
+
+class Arme
+{
+private :
+    std::string m_nomarme;
+    int m_puissancearme;
+public :
+    Arme();
+    Arme( std:: string nomarme, int puissancearme);
+    void changerarme(std::string nomarme, int puissance);
+    void afficher() const;
+    int getpuissance() const;
+};
+#endif
